52dbcb1f5d312863daf4a5bd04a859d5766f3497
Bhagya Madusanka <madusankavpb@gmail.com>
2026-06-18 11:34:08 +0200

    Update outputs.tf

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -2,3 +2,8 @@ output "api_endpoint" {
   description = "The live HTTP URL of the API Gateway used by the frontend application."
   value       = module.api.api_endpoint
 }
+
+output "api_endpoint" {
+  description = "The live HTTP URL of the frontend application."
+  value       = resource.aws_cloudfront_distribution.this.domain_name
+}
